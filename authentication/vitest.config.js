7ba6bf0211b54064bf9d48d02c6d2aac9d1b6062
uo300047 <uo300047@uniovi.es>
2026-04-21 23:17:46 +0200

    vitest authentication

--- authentication/vitest.config.js
+++ authentication/vitest.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
+        globals: true,
+        environment: 'node',
         coverage: {
             provider: 'v8', // you already installed this 👍
             reporter: ['text', 'lcov'], // 👈 THIS is the key
